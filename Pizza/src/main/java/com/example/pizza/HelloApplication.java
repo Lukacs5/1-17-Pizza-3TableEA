38f2444fa5dd00b92b6776949d840c9c83637574
PoganyBenedek <poganybenedek@gmail.com>
2022-12-01 19:31:33 +0100

    Jar update
    Jar update
    
    Co-Authored-By: Gál Lukács <116949893+Lukacs5@users.noreply.github.com>

--- Pizza/src/main/java/com/example/pizza/HelloApplication.java
+++ Pizza/src/main/java/com/example/pizza/HelloApplication.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
---
+++ Pizza/src/main/java/com/example/pizza/Launcher.java
@@ -0,0 +1,11 @@
+package com.example.pizza;
+
+import java.io.IOException;
+
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+        // Replace "Main" with the name of the class that extends Application
+        // See https://stackoverflow.com/a/52654791/3956070 for explanation
+        HelloApplication.main(args);
+    }
+}
